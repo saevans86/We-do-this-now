dd8951975e3d5fed5a11c846b9c38d0efc7beb27
saevans86 <sam_evans1986@hotmail.com>
2023-07-24 09:02:01 -0600

    changed file structure, added vars

---
+++ devlop/js.js
@@ -0,0 +1,10 @@
+var cityName = document.getElementById("cityName");
+var fetchBtn =document.getElementById("fetchBtn");
+var response = document.getElementById("response"); 
+var response = document.getElementById("temp"); 
+var response = document.getElementById("clouds"); 
+var response = document.getElementById("wind"); 
+var response = document.getElementById("precip"); 
+
+
+
