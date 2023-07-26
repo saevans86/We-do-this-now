88871fb1a608e5226975f2e01ef8932392a23328
saevans86 <sam_evans1986@hotmail.com>
2023-07-26 17:38:04 -0600

    pending re-adding precipitation to forecasr for current/future, pending local storage func and style

--- devlop/js.js
+++ devlop/js.js
@@ -6,15 +6,13 @@ var tempEl = document.getElementById("temp");
 var cloudsEl = document.getElementById("clouds"); 
 var windEl = document.getElementById("wind"); 
 var precipEl = document.getElementById("precip"); 
+
 var dateEl = document.getElementById("currentDay");
 var APIKey = "768100cba919239c11d5c616900c6ca5";
 var nextDay = document.getElementById("followingDay");
-var theWeatherT =document.getElementById("theWeatherT");
-var tempT =document.getElementById("tempT");
-var cloudsT = document.getElementById("cloudsT");
-var windT = document.getElementById("windT");
-var precipT = document.getElementById("precipT")
-
+var nextDay1 = document.getElementById("followingDay1");
+var nextDay2 = document.getElementById("followingDay2");
+var nextDay3 = document.getElementById("followingDay3");
 
 
 
@@ -24,31 +22,45 @@ function getApi() {
   fetch(requestUrl)
   .then(function (response ){
     return response.json();
-  }) 
+  }) // need display for current date, future dates with weather
   .then(function (data){    
     var iconCode = data.list[0].weather[0].icon;
     var weatherIconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
     var currentDayIcon = document.getElementById("iconRef");
     responseEl.textContent = "Weather: " + data.list[0].weather[0].description;
-    currentDayIcon.innerHTML = " " + data.list[0].weather[0].icon;
+    currentDayIcon.textContent = " " + data.list[0].weather[0].icon;
     tempEl.textContent = "Temperature: " + data.list[0].main.temp + "°F";
     cloudsEl.textContent = "Clouds: " + data.list[0].clouds.all + "%";
-    windEl.textContent = "Wind: " + data.list[0].wind.speed + " mph";
+    windEl.textContent = "Wind: " + data.list[0].wind.speed + " mph"; // re-add precip
     dateEl.textContent = "Today's date: " + new Date(data.list[0].dt * 1000).toLocaleDateString();
     currentDayIcon.src = weatherIconUrl;
   
     for (let i=1; i <= 4; i++) {
       var forecastIndex  = i * 8;
       let forecastDate = new Date(data.list[forecastIndex].dt * 1000).toLocaleDateString();
+      var iconCode = data.list[0].weather[0].icon;
+      var nextDaysIcon = document.createElement("img");
+      nextDaysIcon.src ="http://openweathermap.org/img/w/" + data.list[forecastIndex].weather[0].icon + ".png";
       var forecastEl =document.createElement("div");
       forecastEl.textContent = "Forecast for:  " + forecastDate + ": ";
       forecastEl.textContent  += "Weather: " + data.list[forecastIndex].weather[0].description;
       forecastEl.textContent += "Temperature: " + data.list[forecastIndex].main.temp + "°F";
       forecastEl.textContent += "Clouds: " +data.list[forecastIndex].clouds.all + "%";
-      forecastEl.textContent += " Wind: " + data.list[forecastIndex].wind.speed + " mph";
-      nextDay.append(forecastEl);
+      forecastEl.textContent += " Wind: " + data.list[forecastIndex].wind.speed + " mph"; //re-add precip
+     
+      forecastEl.appendChild(nextDaysIcon);
+      if (i === 1) {
+        nextDay.appendChild(forecastEl);
+      } else if (i === 2) {
+        nextDay1.appendChild(forecastEl);
+      } else if (i === 3) {
+        nextDay2.appendChild(forecastEl);
+      } else if (i === 4) {
+        nextDay3.appendChild(forecastEl);
+      }
     }
     
+  
     console.log(data);  
   });
   
