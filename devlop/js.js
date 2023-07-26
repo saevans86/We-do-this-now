cda555f732edee78f808def1e634ad212f7c51f7
saevans86 <sam_evans1986@hotmail.com>
2023-07-26 10:46:34 -0600

    pending local storage, styling

--- devlop/js.js
+++ devlop/js.js
@@ -6,30 +6,52 @@ var tempEl = document.getElementById("temp");
 var cloudsEl = document.getElementById("clouds"); 
 var windEl = document.getElementById("wind"); 
 var precipEl = document.getElementById("precip"); 
-var TOPSECRETAPIKEY; // will need to update code before push, saved into secret key with github/ 
-
+var dateEl = document.getElementById("currentDay");
+var APIKey = "768100cba919239c11d5c616900c6ca5";
+var nextDay = document.getElementById("followingDay");
+var theWeatherT =document.getElementById("theWeatherT");
+var tempT =document.getElementById("tempT");
+var cloudsT = document.getElementById("cloudsT");
+var windT = document.getElementById("windT");
+var precipT = document.getElementById("precipT")
 
 
 
 
 function getApi() {  
   city = userInputEl.value;
-  var requestUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey + "&units=imperial";
+  var requestUrl = "http://api.openweathermap.org/data/2.5/forecast?q=" + city + "&appid=" + APIKey + "&units=imperial";
   fetch(requestUrl)
   .then(function (response ){
     return response.json();
-  })
-  .then(function (data){ 
-    responseEl.textContent = "theWeather: " + data.weather[0].description;
-      tempEl.textContent = "temp: " + data.main.temp + "°F";
-      cloudsEl.textContent = "clouds: " + data.clouds.all + "%";
-      windEl.textContent = "wind: " + data.wind.speed + " mph";
-      precipEl.textContent = "precip: " + (data.rain ? data.rain["1h"] + " mm" : "0 mm");
-        { 
+  }) 
+  .then(function (data){    
+    var iconCode = data.list[0].weather[0].icon;
+    var weatherIconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
+    var currentDayIcon = document.getElementById("iconRef");
+    responseEl.textContent = "Weather: " + data.list[0].weather[0].description;
+    currentDayIcon.innerHTML = " " + data.list[0].weather[0].icon;
+    tempEl.textContent = "Temperature: " + data.list[0].main.temp + "°F";
+    cloudsEl.textContent = "Clouds: " + data.list[0].clouds.all + "%";
+    windEl.textContent = "Wind: " + data.list[0].wind.speed + " mph";
+    dateEl.textContent = "Today's date: " + new Date(data.list[0].dt * 1000).toLocaleDateString();
+    currentDayIcon.src = weatherIconUrl;
+  
+    for (let i=1; i <= 4; i++) {
+      var forecastIndex  = i * 8;
+      let forecastDate = new Date(data.list[forecastIndex].dt * 1000).toLocaleDateString();
+      var forecastEl =document.createElement("div");
+      forecastEl.textContent = "Forecast for:  " + forecastDate + ": ";
+      forecastEl.textContent  += "Weather: " + data.list[forecastIndex].weather[0].description;
+      forecastEl.textContent += "Temperature: " + data.list[forecastIndex].main.temp + "°F";
+      forecastEl.textContent += "Clouds: " +data.list[forecastIndex].clouds.all + "%";
+      forecastEl.textContent += " Wind: " + data.list[forecastIndex].wind.speed + " mph";
+      nextDay.append(forecastEl);
     }
-       
+    
     console.log(data);  
   });
+  
   
 }
 
