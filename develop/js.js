04bf881a2b3f5e50cc8e8e2797ab18f053ced8ec
saevans86 <sam_evans1986@hotmail.com>
2023-07-27 08:57:22 -0600

    pending localstorage func, styling

--- develop/js.js
+++ develop/js.js
@@ -1,14 +1,12 @@
 var userInputEl = document.getElementById("userInput");
-var city = "";
 var fetchBtnEl =document.getElementById("fetchButton");
 var responseEl = document.getElementById("theWeather"); 
 var tempEl = document.getElementById("temp"); 
-var cloudsEl = document.getElementById("clouds"); 
+var cloudEl = document.getElementById("clouds"); 
 var windEl = document.getElementById("wind"); 
 var precipEl = document.getElementById("precip"); 
-
 var dateEl = document.getElementById("currentDay");
-var APIKey = "768100cba919239c11d5c616900c6ca5";
+
 var nextDay = document.getElementById("followingDay");
 var nextDay1 = document.getElementById("followingDay1");
 var nextDay2 = document.getElementById("followingDay2");
@@ -22,7 +20,7 @@ function getApi() {
   fetch(requestUrl)
   .then(function (response ){
     return response.json();
-  }) // need display for current date, future dates with weather
+  }) 
   .then(function (data){    
     var iconCode = data.list[0].weather[0].icon;
     var weatherIconUrl = "http://openweathermap.org/img/w/" + iconCode + ".png";
@@ -30,8 +28,9 @@ function getApi() {
     responseEl.textContent = "Weather: " + data.list[0].weather[0].description;
     currentDayIcon.textContent = " " + data.list[0].weather[0].icon;
     tempEl.textContent = "Temperature: " + data.list[0].main.temp + "°F";
-    cloudsEl.textContent = "Clouds: " + data.list[0].clouds.all + "%";
-    windEl.textContent = "Wind: " + data.list[0].wind.speed + " mph"; // re-add precip
+    cloudEl.textContent = "Clouds: " + data.list[0].clouds.all + "%";
+    windEl.textContent = "Wind: " + data.list[0].wind.speed + " mph"; 
+    precipEl.textContent = "Chance of rain: " + (data.list[0].rain ? data.list[0].rain.description["1h"] + " mm" : "0 mm");
     dateEl.textContent = "Today's date: " + new Date(data.list[0].dt * 1000).toLocaleDateString();
     currentDayIcon.src = weatherIconUrl;
   
@@ -43,11 +42,13 @@ function getApi() {
       nextDaysIcon.src ="http://openweathermap.org/img/w/" + data.list[forecastIndex].weather[0].icon + ".png";
       var forecastEl =document.createElement("div");
       forecastEl.textContent = "Forecast for:  " + forecastDate + ": ";
-      forecastEl.textContent  += "Weather: " + data.list[forecastIndex].weather[0].description;
-      forecastEl.textContent += "Temperature: " + data.list[forecastIndex].main.temp + "°F";
-      forecastEl.textContent += "Clouds: " +data.list[forecastIndex].clouds.all + "%";
-      forecastEl.textContent += " Wind: " + data.list[forecastIndex].wind.speed + " mph"; //re-add precip
-     
+      forecastEl.textContent  += "Weather:  " + data.list[forecastIndex].weather[0].description;
+      forecastEl.textContent += "Temperature:  " + data.list[forecastIndex].main.temp + "°F";
+      forecastEl.textContent += "Clouds:  " +data.list[forecastIndex].clouds.all + "%";
+      forecastEl.textContent += " Wind:  " + data.list[forecastIndex].wind.speed + " mph";
+      forecastEl.textContent += "Chance of rain: " + (data.list[forecastIndex].rain ? data.list[forecastIndex].rain["1h"] + " mm" : "0 mm");
+      
+
       forecastEl.appendChild(nextDaysIcon);
       if (i === 1) {
         nextDay.appendChild(forecastEl);
@@ -59,15 +60,13 @@ function getApi() {
         nextDay3.appendChild(forecastEl);
       }
     }
-    
+  
+  
+
   
     console.log(data);  
   });
-  
-  
-}
 
 
 fetchBtnEl.addEventListener("click", getApi);
-
-  
+}
